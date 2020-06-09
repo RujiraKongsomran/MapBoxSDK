09c106b4fd3b4f0945e86dc11bc1edeced1ab6a9
Rujira Kongsomran <biwberry@outlook.com>
2020-06-09 23:48:50 +0700

    Show map and add permission in AndroidManifest.xml

--- app/src/main/java/com/rujirakongsomran/mapboxsdk/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/mapboxsdk/MainActivity.java
@@ -1,14 +1,62 @@
 package com.rujirakongsomran.mapboxsdk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, "pk.eyJ1IjoiYW5kcm9pZHNwcmVhZCIsImEiOiJja2I4MmJ1Y3cwMDNjMnhxdGd2dmFicjlkIn0.Iq2woFinJLy2jnBIpM0Ufg");
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
